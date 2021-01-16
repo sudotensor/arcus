98092def42648d31ec4d9d28155aa703f6701a2d
oleg-yurchenko <oleg@bgmot.com>
2021-01-16 10:55:24 -0500

    this commit will add random image request form unsplash. No API key has been provided yet so the tab is eternally stuck on loading (application can still do other things while image loads)

--- lib/random_unsplash.dart
+++ lib/random_unsplash.dart
@@ -1,13 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class RandomUnsplash extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        
-      )
+class RandImg {
+  final String url;
+
+  RandImg({this.url});
+
+  factory RandImg.fromJson(Map<String, dynamic> json) {
+    return RandImg(
+      url: json["urls"]["raw"],
     );
   }
 }
 
+Future<RandImg> fetchImg() async {
+  final response = await http.get("https://api.unsplash.com/photos/random");
+
+  if (response.statusCode == 200) {
+    return RandImg.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception("Could not load Random Image");
+  }
+}
+
+class RandomUnsplash extends StatefulWidget {
+  @override
+  _RandomUnsplashState createState() => _RandomUnsplashState();
+}
+
+class _RandomUnsplashState extends State<RandomUnsplash> {
+  Future<RandImg> futureImg;
+
+  @override
+  void initState() {
+    super.initState();
+    futureImg = fetchImg();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          FutureBuilder<RandImg>(
+            future: futureImg,
+            builder: (context, snapshot) {
+              if(snapshot.hasData) {
+                return Image.network(snapshot.data.url);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              return CircularProgressIndicator();
+            },
+          ),
+          OutlinedButton(
+            onPressed: () {
+              futureImg = fetchImg();
+            },
+            child: Icon(Icons.refresh),
+          )
+        ],
+      ),
+    );
+  }
+}

8035a3d8292d10959c10f1c45869f6d5e1e56cee
Davis Liu <d275liu@uwaterloo.ca>
2021-01-16 10:21:35 -0500

    Update tabs.dart

--- lib/tabs.dart
+++ lib/tabs.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
 import './local_image_picker.dart';
 import './favourites.dart';
-<<<<<<< HEAD
 import './random_unsplash.dart';
-=======
-import 'favourites.dart';
->>>>>>> ddb219543d456b65c148c703223c40e54ac7fd9a
 
 class MainPage extends StatelessWidget {
   @override

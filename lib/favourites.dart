86a54822d319fdfd2006656df7dd34a4ba071d71
Davis Liu <d275liu@uwaterloo.ca>
2021-01-16 11:24:28 -0500

    Update favourites.dart

--- lib/favourites.dart
+++ lib/favourites.dart
@@ -6,26 +6,34 @@ class Favourites extends StatefulWidget {
 }
 
 class _FavouritesState extends State<Favourites> {
-  final List<String> favourites = <String>['Palette 1', 'Palette 2', 'Palette 3', 'Palette 4'];
+  static const List<_MyPalette> palettes = const <_MyPalette>[
+    _MyPalette('Palette1', const Color(0xFFFFC100), const Color(0xFF91FAFF), const Color(0xFF00BCFF), const Color(0xFF009BEE)),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
         padding: const EdgeInsets.all(8),
-        itemCount: favourites.length,
+        itemCount: palettes.length,
         itemBuilder: (BuildContext context, int index) {
           return Container(
             height: 70,
             color: Colors.white,
             child: Align(
-                alignment: Alignment(-0.9, 0.0),
+                alignment: Alignment(0.0, 0.0),
                 child: Row(
                   children: <Widget> [
-                    Text('${favourites[index]}'),
-                    Icon(Icons.crop_square_sharp),
-                    Icon(Icons.crop_square_sharp),
-                    Icon(Icons.crop_square_sharp),
-                    Icon(Icons.crop_square_sharp),
+                    Padding(padding: EdgeInsets.all(8.0)),
+                    Text('${palettes[index].name}'),
+                    Spacer(),
+                    Box(palettes[index].color1),
+                    Padding(padding: EdgeInsets.all(8.0)),
+                    Box(palettes[index].color2),
+                    Padding(padding: EdgeInsets.all(8.0)),
+                    Box(palettes[index].color3),
+                    Padding(padding: EdgeInsets.all(8.0)),
+                    Box(palettes[index].color4),
+                    Padding(padding: EdgeInsets.all(8.0)),
                   ]
                 )
             ),
@@ -33,4 +41,34 @@ class _FavouritesState extends State<Favourites> {
         },
     );
   }
+}
+
+class Box extends StatelessWidget {
+  Box(this.color);
+
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 20,
+      width: 20.0,
+      color: Colors.transparent,
+      child: Container(
+        decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.all(Radius.circular(10.0))),
+      ),
+    );
+  }
+}
+
+class _MyPalette {
+  const _MyPalette(this.name, this.color1, this.color2, this.color3, this.color4);
+
+  final String name;
+  final Color color1;
+  final Color color2;
+  final Color color3;
+  final Color color4;
 }

34ee8d65cf0241dfed52990c83175b586a14fd17
Davis Liu <d275liu@uwaterloo.ca>
2021-01-17 04:09:25 -0500

    Palette page display colors

--- lib/palette_page.dart
+++ lib/palette_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:quartet/quartet.dart';
 
 class PalettePage extends StatelessWidget {
   PalettePage(this.colors);
@@ -12,22 +14,175 @@ class PalettePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Palette Page"),
       ),
-      body: Center(
-        child: FlatButton(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0)),
-          color: Colors.black,
-          child: new Text(
-            'Go back',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white.withOpacity(1),
-            ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Spacer(),
+          Row(
+            children: <Widget> [
+              Padding(padding: EdgeInsets.all(8.0)),
+              Flexible(
+                  flex: 1,
+                  child: Container(
+                      padding: EdgeInsets.all(4),
+                      height: 170.0,
+                      color: Colors.transparent,
+                      child: Container(
+                          decoration: BoxDecoration(
+                            color: colors != null ? colors[0] : Colors.transparent,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                          )))),
+              Flexible(
+                  flex: 1,
+                  child: Container(
+                      padding: EdgeInsets.all(4),
+                      height: 170.0,
+                      color: Colors.transparent,
+                      child: Container(
+                          decoration: BoxDecoration(
+                            color: colors != null ? colors[2] : Colors.transparent,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                          )))),
+              Flexible(
+                  flex: 1,
+                  child: Container(
+                      padding: EdgeInsets.all(4),
+                      height: 170.0,
+                      color: Colors.transparent,
+                      child: Container(
+                          decoration: BoxDecoration(
+                            color: colors != null ? colors[2] : Colors.transparent,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                          )))),
+              Flexible(
+                  flex: 1,
+                  child: Container(
+                      padding: EdgeInsets.all(4),
+                      height: 170.0,
+                      color: Colors.transparent,
+                      child: Container(
+                          decoration: BoxDecoration(
+                            color: colors != null ? colors[3] : Colors.transparent,
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                          )))),
+              Padding(padding: EdgeInsets.all(8.0)),
+            ],
           ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
+          Row(
+            children: <Widget> [
+              Padding(padding: EdgeInsets.all(8.0)),
+              Flexible(
+                  flex: 1,
+                  child: Container(
+                    height: 30.0,
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    color: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 6),
+                      child: FittedBox(
+                       child: Text('${slice(slice('${ColorToHex(colors[0])}', 6), 1, -1)}', style: TextStyle(fontSize: 40)),
+                      ),
+                    ),
+                  )),
+              Flexible(
+                  flex: 1,
+                  child: Container(
+                    height: 30.0,
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    color: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 6),
+                      child: FittedBox(
+                        child: Text('${slice(slice('${ColorToHex(colors[1])}', 6), 1, -1)}', style: TextStyle(fontSize: 40)),
+                      ),
+                    ),
+                  )),
+              Flexible(
+                  flex: 1,
+                  child: Container(
+                    height: 30.0,
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    color: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 6),
+                      child: FittedBox(
+                        child: Text('${slice(slice('${ColorToHex(colors[2])}', 6), 1, -1)}', style: TextStyle(fontSize: 40)),
+                      ),
+                    ),
+                  )),
+              Flexible(
+                  flex: 1,
+                  child: Container(
+                    height: 30.0,
+                    padding: EdgeInsets.symmetric(horizontal: 8),
+                    color: Colors.transparent,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                      padding: EdgeInsets.symmetric(horizontal: 6),
+                      child: FittedBox(
+                        child: Text('${slice(slice('${ColorToHex(colors[3])}', 6), 1, -1)}', style: TextStyle(fontSize: 40)),
+                      ),
+                    ),
+                  )),
+              Padding(padding: EdgeInsets.all(8.0)),
+            ],
+          ),
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FlatButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0)),
+                color: Colors.black,
+                child: new Text(
+                  'Submit',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(1),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+          Padding(padding: EdgeInsets.all(20.0)),
+          Row (
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+
+            ],
+          ),
+        ],
       ),
     );
   }

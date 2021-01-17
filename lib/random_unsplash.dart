36a35ef05b8215621e8738c268c3a6547e1621cd
Angelina <angielam1223@hotmail.com>
2021-01-17 03:00:19 -0500

    made buttons look like the main page (kinda)

--- lib/random_unsplash.dart
+++ lib/random_unsplash.dart
@@ -240,7 +240,25 @@ class _RandomUnsplashState extends State<RandomUnsplash> {
                 ),
               ]
           ),
-
+          FlatButton(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0)),
+            color: Colors.black,
+            child: new Text(
+              'Add to Favourites',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white.withOpacity(1),
+              ),
+            ),
+            //onPressed: getImage,
+            onPressed: (){
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => PalettePage()),
+              );
+            },
+          ),
           new FlatButton(
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(8.0)),
@@ -259,6 +277,34 @@ class _RandomUnsplashState extends State<RandomUnsplash> {
             },
           ),
         ],
+      ),
+    );
+  }
+}
+
+class PalettePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Palette Page"),
+      ),
+      body: Center(
+        child: FlatButton(
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8.0)),
+          color: Colors.black,
+          child: new Text(
+            'Go back',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white.withOpacity(1),
+            ),
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
     );
   }

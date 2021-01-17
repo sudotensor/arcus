587320c8a41deb5b61902640b6fcb5b2cf2f73a8
oleg-yurchenko <oleg@bgmot.com>
2021-01-17 03:34:57 -0500

    unsplashed_change

--- lib/search_unsplash.dart
+++ lib/search_unsplash.dart
@@ -152,31 +152,3 @@ class _SearchUnsplashState extends State<SearchUnsplash> {
     ]);
   }
 }
-
-class PalettePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Palette Page"),
-      ),
-      body: Center(
-        child: FlatButton(
-          shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0)),
-          color: Colors.black,
-          child: new Text(
-            'Go back',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white.withOpacity(1),
-            ),
-          ),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-    );
-  }
-}
